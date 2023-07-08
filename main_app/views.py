4df70a3d58b14ea39b4de1b4910808cb5b0735f6
Brandonbay <undftdcreed@gmail.com>
2023-07-08 09:32:10 -0700

    added views template

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+class Home(View):
+    
+    def get(self, request):
+        return HttpResponse("snapshoot home")
+    
+class About(View):
+    def get(self, request):
+        return HttpResponse("snapshoot about")
