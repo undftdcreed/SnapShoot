20dcf755f73af1dae09aa8f9c4a90c38132f80b7
Brandonbay <undftdcreed@gmail.com>
2023-07-15 15:51:38 -0700

    html for registration

--- snap_shoot/settings.py
+++ snap_shoot/settings.py
@@ -152,3 +152,6 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
+
+
+LOGIN_REDIRECT_URL = '/artists/'
