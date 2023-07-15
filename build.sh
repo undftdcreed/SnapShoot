b26fe2188d4a6d9f19d3106575058cced79a41a7
Brandonbay <undftdcreed@gmail.com>
2023-07-15 07:40:04 -0700

    psycopg2-binary

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 pip install django
 pip install dj_database_url
 pip install whitenoise
+pip install psycopg2-binary
 
 python manage.py collectstatic --no-input
 python manage.py migrate
