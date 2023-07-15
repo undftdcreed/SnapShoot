d7f16ac726a7f5e0303b73c6a38b0a15b00c4483
Brandonbay <undftdcreed@gmail.com>
2023-07-15 11:25:54 -0700

    changes

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,19 +1,19 @@
-$(".navbar-burger").click(function () {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
+
   
 
- // Get the theme switcher input elements
-const themeInputs = document.querySelectorAll('input[name="themes"]');
-
-// Handle theme change
-themeInputs.forEach((input) => {
-  input.addEventListener('change', () => {
-    if (input.checked) {
-      const selectedTheme = input.id;
-      document.documentElement.className = selectedTheme + '-theme';
-    }
+  $(document).ready(function() {
+    $(".navbar-burger").click(function() {
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+    });
+  
+    // Handle theme change
+    $('input[name="themes"]').change(function() {
+      if (this.checked) {
+        const selectedTheme = this.id;
+        document.documentElement.className = selectedTheme + "-theme";
+      }
+    });
   });
-});
+  
