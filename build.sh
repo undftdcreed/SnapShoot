a7c0cc4bbe10324e85f0a7fdac3b30f8e829cd54
Brandonbay <undftdcreed@gmail.com>
2023-07-18 13:16:25 -0700

    yee

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ pip install dj_database_url
 pip install whitenoise
 pip install psycopg2-binary
 pip install gunicorn
-
+python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
