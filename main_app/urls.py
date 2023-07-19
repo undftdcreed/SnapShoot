3522ce4d6a13ba37d2506857a82f2b5a0498b2cf
Brandonbay <undftdcreed@gmail.com>
2023-07-19 11:03:09 -0700

    home file structure change

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('accounts/signup/', views.Signup.as_view(), name="signup"),
     path('listings/', views.ListingList.as_view(), name="listing_list"),
     path('listings/new/', views.ListingCreate.as_view(), name="listing_create"),
     path('listings/<int:pk>/', views.ListingDetail.as_view(), name="listing_detail"),
     path('listings/<int:pk>/update', views.ListingUpdate.as_view(), name="listing_update"),
     path('listings/<int:pk>/delete', views.ListingDelete.as_view(), name="listing_delete"),
-    path('accounts/signup/', views.Signup.as_view(), name="signup"),
     path('listings/<int:listing_pk>/booking/', views.BookingCreate.as_view(), name='booking_create'),
     path('bookings/<int:pk>/', views.BookingDetail.as_view(), name='booking_detail'),
     path('bookings/<int:pk>/update/', views.BookingUpdate.as_view(), name='booking_update'),
