c66430ccd32b66a7b0bdfaf0791fb8bb054cab4b
Brandonbay <undftdcreed@gmail.com>
2023-07-19 12:07:31 -0700

    test

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('listings/<int:listing_pk>/booking/', views.BookingCreate.as_view(), name='booking_create'),
     path('bookings/<int:pk>/', views.BookingDetail.as_view(), name='booking_detail'),
     path('bookings/<int:pk>/update/', views.BookingUpdate.as_view(), name='booking_update'),
+    path('bookings/<int:pk>/update/', views.BookingUpdate.as_view(), name='booking_update'),
+
 ]
